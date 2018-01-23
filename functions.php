d6a79492d21936d73eef68573687a8674c3583e2
Mike Jandreau <mikejandreau@gmail.com>
2018-01-23 17:00:09 -0500

    Updated style/script file calls

--- functions.php
+++ functions.php
@@ -117,15 +117,40 @@ add_action( 'widgets_init', 'baseinstallfive_widgets_init' );
  * Enqueue scripts and styles.
  */
 function baseinstallfive_scripts() {
-	wp_enqueue_style( 'baseinstallfive-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'baseinstallfive-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// minified stylesheet
+	wp_enqueue_style( 'baseinstallfive-min-style', get_template_directory_uri() . '/assets/css/styles.css', array(), time() ); 
+	
+	// theme styles
+	// wp_enqueue_style( 'baseinstallfive-css', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'baseinstallfive-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// theme scripts
+	wp_enqueue_script( 'baseinstallfive-js', get_template_directory_uri() . '/assets/js/scripts.js', array(), '20151215', true ); 
+
+
+
+
+
+	// wp_enqueue_style( 'baseinstallfive-style', get_stylesheet_uri() );
+
+	// wp_enqueue_script( 'baseinstallfive-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	// wp_enqueue_script( 'baseinstallfive-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+
+
+
+
+
+
+
+
+
+
 }
 add_action( 'wp_enqueue_scripts', 'baseinstallfive_scripts' );
 
